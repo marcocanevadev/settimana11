ef574ee715bce4115c504aafaa8e3ec02aaab528
marco caneva <marco.caneva.dev@outlook.com>
2023-05-22 13:16:58 +0200

    added items attribute

--- es3.py
+++ es3.py
@@ -4,21 +4,43 @@ define sorted list with no duplicates
 class SortedSet(list):
     def __init__(self,iterable):
       #  iterable = set(iterable)
-        super().__init__(set(iterable))
+        #super().__init__(set(iterable))
+        self._items= sorted(set(iterable))
        # self = set(self)
     def insert(self, value):
-       # print(self)
-        if value not in self:
-            super().append(value)
-            self = sorted(set(self))
-            return self
-          #  _index = [i for x,i in self if x < value]
-           # super().insert(_index,value)
-            
+      # print(self)
+      if super().count(value)==0:
+    
+          self._items.append(value)
+          self._items.sort()
+      """
+      if value not in self:
+          print('inserting..')
+          super().append(value)
+          print('self: ',self)
+          print('sorted', sorted(set(self)))
+          #self = sorted(set(self))
+          return sorted(set(self))
+        #  _index = [i for x,i in self if x < value]
+          # super().insert(_index,value)
+      """
+      return self._items
     def append(self, value):
         return SortedSet.insert(self,value)
+    
+    def extend(self, other):
+        return set(other)
+    
+    def reverse(self):
+        raise RuntimeError
+    
+    def indexAfter(self,value):
+        print(type(self._items))
+        print([v for x, v in self._items if x > value][0])
+
 
 if __name__ == '__main__':
+    """
     l = [2,1,8,5,3,1,1]
     print(SortedSet(l))
     print('\t',l)
@@ -26,9 +48,13 @@ if __name__ == '__main__':
     print('\t',l)
     print(SortedSet.append(l,7))
     print('\t',l)
-
-    list = SortedSet([2,3,3,1])
+  """
+    list = SortedSet([5,2,3,3,1])
     print(list)
+   
     list.insert(0)
     list.append(6)
-    print(list)
+    print(list._items) 
+    
+    i = list.indexAfter(4)
+    print(i)
